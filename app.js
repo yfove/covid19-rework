960cc67eb75c09770b929c80a26d5ba6ab404115
cfue <curtis.ch.chat@gmail.com>
2020-03-24 23:30:26 -0600

    corona virust statistics API

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const name = "curt";
+fetch("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+    "x-rapidapi-key": "395feb7c3emshb4a91900aa9f376p161dc0jsn52d04d06ce2e"
+  }
+})
+  .then(response => response.json()) // (generic response) getting the actual data we have to convert to .json
+  .then(data => {
+    console.log(data); //we always want to start with console.log(data)
+  });
+
+// console.log(stats);
